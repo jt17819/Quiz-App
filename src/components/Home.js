d11d8ee219dfe55ac831edd893382173c584cfae
amirasif01 <amir_12@hotmail.co.uk>
2022-02-07 17:47:32 +0000

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,59 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import Question from "./Question";
+export const apiContext = React.createContext();
 
 const Home = ({ categories }) => {
-    let navigate = useNavigate();
+  const [apiData, setApiData] = useState({ amount: 10 });
+  let navigate = useNavigate();
 
-const handleSubmit = e => {
-    e.preventDefault()
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
     const apiOptions = {
-        players: e.target.players.value,
-        category: e.target.category.value,
-        difficulty: e.target.difficulty.value
-    }
+      //   players: e.target.players.value,
+      amount: 10,
+      category: parseInt(e.target.category.value),
+      difficulty: e.target.difficulty.value,
+    };
 
-    navigate('/quiz');
-    console.log(apiOptions)
+    navigate("/quiz");
+    console.log(apiOptions);
+    setApiData(apiOptions);
+  };
 
-    
-}
+  return (
+    <apiContext.Provider value={{ apiData }}>
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="players">Players</label>
+          <select id="players">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
 
-    return (
-        <div className='formContainer'>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='players'>Players</label>
-                <select id='players'
-                >
-                    <option  value='1'>1</option>
-                    <option  value='2'>2</option>
-                </select>
+          <label htmlFor="category">Category</label>
+          <select id="category">
+            {categories.map((category) => (
+              <option value={category.id} key={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
 
-                <label htmlFor='category'>Category</label>
-                <select id='category'
-                >
-                    {categories.map(category => (
-                        <option value={category.id} key={category.id}>
-                            {category.name}
-                        </option>
-                    ))}
-                </select>
-
-                <label htmlFor='difficulty'>Difficulty</label>
-                <select id='difficulty'
-                >
-                    <option value=''>Any Difficulty</option>
-                    <option value='easy'>Easy</option>
-                    <option value='medium'>Medium</option>
-                    <option value='hard'>Hard</option>
-                </select>
-                <input type='submit' id='startQuiz' value='Start'/>
-
-            </form>
-        </div>
-    )
+          <label htmlFor="difficulty">Difficulty</label>
+          <select id="difficulty">
+            <option value="">Any Difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+          <input type="submit" id="startQuiz" value="Start" />
+        </form>
+      </div>
+      <Question />
+    </apiContext.Provider>
+  );
 };
 
 export default Home;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,56 +1,101 @@
-const handleSubmit = async (e) => {
-  e.preventDefault();
+import { apiContext } from "./Home";
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
 
-  resetGame();
+const Question = () => {
+  const [questionBank, setQuestionsBank] = useState([]);
+  const { apiData } = useContext(apiContext);
+  console.log(apiData);
+  useEffect(() => {
+    const getQuestions = async (apiData) => {
+      try {
+        const { data } = await axios({
+          method: "GET",
+          url: `https://opentdb.com/api.php`,
+          params: { ...apiData },
+        });
 
-  if (apiOptions.category) {
-    setCurrentCategory(
-      categories.find(
-        (category) => category.id === parseInt(apiOptions.category)
-      ).name
-    );
-  }
+        setQuestionsBank(
+          await data.results.map((questionItem, index) => {
+            const options = [
+              ...questionItem.incorrect_answers,
+              questionItem.correct_answer,
+            ];
+            return {
+              id: `${index}-${Date.now()}`,
+              question: questionItem.question,
+              answer: questionItem.correct_answer,
+              options: options.sort(() => Math.random() - 0.5),
+            };
+          })
+        );
+        console.log(questionBank);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getQuestions(apiData);
+  }, [apiData, questionBank]);
 
-  const [loadingQuestions, setLoadingQuestions] = useState(false);
-
-  setLoadingQuestions(true);
-  try {
-    const { data } = await axios({
-      method: "GET",
-      url: "https://opentdb.com/api.php?amount=10",
-      // params: { ...apiOptions },
-    });
-
-    if (!data.results.length) {
-      setError(
-        "🙁 No questions found with the selected options. please try again!"
-      );
-      setLoadingQuestions(false);
-      return;
-    }
-
-    setQuestionsBank(
-      data.results.map((questionItem, index) => {
-        const answer = decodeString(questionItem.correct_answer);
-        const wrongAnswers = [
-          ...questionItem.incorrect_answers.map((a) => decodeString(a)),
-          answer,
-        ];
-        return {
-          id: `${index}-${Date.now()}`,
-          question: decodeString(questionItem.question),
-          answer: answer,
-          options: wrongAnswers.sort(() => Math.random() - 0.5),
-        };
-      })
-    );
-    setTotalQuestions(data.results.length);
-    setQuizInProgress(true);
-  } catch (error) {
-    console.log(error);
-    setError(
-      "🙁 Error loading questions from the API. Please try again later."
-    );
-  }
-  setLoadingQuestions(false);
+  return <div>hello</div>;
 };
+
+// const handleSubmit = async (e) => {
+//   e.preventDefault();
+
+//   resetGame();
+
+//   if (apiOptions.category) {
+//     setCurrentCategory(
+//       categories.find(
+//         (category) => category.id === parseInt(apiOptions.category)
+//       ).name
+//     );
+//   }
+
+//   const [loadingQuestions, setLoadingQuestions] = useState(false);
+
+//   setLoadingQuestions(true);
+//   try {
+//     const { data } = await axios({
+//       method: "GET",
+//       url: "https://opentdb.com/api.php?amount=10",
+//       // params: { ...apiOptions },
+//     });
+
+//     if (!data.results.length) {
+//       setError(
+//         "🙁 No questions found with the selected options. please try again!"
+//       );
+//       setLoadingQuestions(false);
+//       return;
+//     }
+
+//     setQuestionsBank(
+//       data.results.map((questionItem, index) => {
+//         const answer = decodeString(questionItem.correct_answer);
+//         const wrongAnswers = [
+//           ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+//           answer,
+//         ];
+//         return {
+//           id: `${index}-${Date.now()}`,
+//           question: decodeString(questionItem.question),
+//           answer: answer,
+//           options: wrongAnswers.sort(() => Math.random() - 0.5),
+//         };
+//       })
+//     );
+//     setTotalQuestions(data.results.length);
+//     setQuizInProgress(true);
+//   } catch (error) {
+//     console.log(error);
+//     setError(
+//       "🙁 Error loading questions from the API. Please try again later."
+//     );
+//   }
+//   setLoadingQuestions(false);
+// };
+
+export default Question;
