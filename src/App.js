ea432e1ca184f827aa99a0b9432b0f48daf7c27d
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2022-02-07 14:09:34 +0000

    form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React,{useEffect, useState } from 'react';
 import './App.css';
 import { Outlet, Link } from "react-router-dom";
 import Home from './components/Home'
+import axios from 'axios'
+
+
 
 function App() {
+  const [loadingCategories, setLoadingCategories] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoadingCategories(true)
+
+    let cancel
+
+      axios({
+        method: 'GET',
+        url: 'https://opentdb.com/api_category.php',
+        cancelToken: new axios.CancelToken(c => (cancel = c)),
+
+        
+      }).then(({ data }) => {
+        setCategories(data.trivia_categories)
+				setLoadingCategories(false)
+      }).catch(error => {
+				if (axios.isCancel(error)) return
+				console.log(error)
+				setLoadingCategories(false)
+				setError(
+					'Error loading categories'
+				)
+  })
+  return () => cancel() },
+  
+  [])
+     
   return (
     <div className="App">
       <header>
@@ -13,7 +46,7 @@ function App() {
           <Link to='/leaderboard'>Leaderboard</Link>
         </nav>
       </header>
-      <Home />
+      <Home categories={categories}/>
     <Outlet/>
     </div>
   );
