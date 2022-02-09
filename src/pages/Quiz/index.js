8aaa81d4e98751803ec18913e365f2935fc83a61
amirasif01 <amir_12@hotmail.co.uk>
2022-02-09 15:49:58 +0000

    scores and players

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 // import "./style.css";
 import { AnswerCard, Question } from "../../components";
-import { useSelector} from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { moveToNextQuestion } from "../../actions";
 const Quiz = () => {
-  
-  
-    const currentQuestion = useSelector((state) => state.quizReducer.current_question_index);
-    const result = useSelector((state) => state.quizReducer.results);
-    const answers = result[currentQuestion].answers;
-    const index = result.indexOf(result[currentQuestion])
-    const question = result[currentQuestion].question;
+  const dispatch = useDispatch();
+  const currentQuestion = useSelector(
+    (state) => state.quizReducer.current_question_index
+  );
+  const result = useSelector((state) => state.quizReducer.results);
+  const answers = result[currentQuestion].answers;
+  // const index = result.indexOf(result[currentQuestion]);
+  const question = result[currentQuestion].question;
+  const players = useSelector((state) => state.user.user.username);
+  const playerTurn = useSelector((state) => state.quizReducer.playerTurn);
 
-    // SHUFFLE ARRAY, so answers are not in the same order each time
+ 
+  if (playerTurn === players.length) {
+    dispatch(moveToNextQuestion(["test", 0]));
+  }
+  // SHUFFLE ARRAY, so answers are not in the same order each time
   function randomAnswers(array) {
     var currentIndex = array.length,
       temporaryValue,
@@ -30,18 +37,24 @@ const Quiz = () => {
     return array;
   }
 
-//Use the shuffled array and for each answer in the array map over it 
+  //Use the shuffled array and for each answer in the array map over it
   const shuffledAnswers = randomAnswers(answers);
- 
+
   return (
     <div role="quiz-container" id="quiz-page">
-      <Question question={question} index={index} />
+      <p>{players[playerTurn]} it's your turn</p>
+      <Question question={question} index={currentQuestion} />
 
       {shuffledAnswers &&
-        shuffledAnswers.map((answer) => <AnswerCard answer={answer} index={index} />)} 
+        shuffledAnswers.map((answer) => (
+          <AnswerCard
+            answer={answer}
+            index={currentQuestion}
+            playerTurn={playerTurn}
+          />
+        ))}
     </div>
   );
-  
 };
 
 export default Quiz;
--- src/reducers/quizReducer.js
+++ src/reducers/quizReducer.js
@@ -2,16 +2,20 @@ const init = {
   loading: false,
   results: [{ question: "", correct_answer: "", answers: [] }],
   current_question_index: 0,
-  score: 0,
+  score: [],
+  playerTurn: 0,
 };
 
 const quizReducer = (state = init, action) => {
   //when refering to index, I mean index of the question in the array
-  const chosenAnswer = action.payload;
+  const chosenAnswer = action.payload ? action.payload[0] : "";
+  const playerTurn = action.payload ? action.payload[1] : 0;
+  // const chosenAnswer = action.payload;
+  // const playerTurn = 0;
   const currentQuestionIndex = state.current_question_index;
   const correctAnswer = state.results[currentQuestionIndex].correct_answer;
   const nextQuestionIndex = state.current_question_index + 1;
-  const score = state.score;
+  // const score = state.score;
 
   switch (action.type) {
     case "LOAD_QUESTIONS":
@@ -24,24 +28,54 @@ const quizReducer = (state = init, action) => {
       return { ...state, error: action.payload };
 
     case "CHANGE_QUESTION":
+      console.log(action.payload);
       //at this point when change question, we want to know whether the answer they clicked was correct
       // if chosenAnswer and correct answer are the same move to next question but also add one to the score
       if (chosenAnswer === correctAnswer) {
+        const scoreArray = [...state.score];
+        const newScore = state.score[playerTurn]
+          ? state.score[playerTurn] + 1
+          : 1;
+        scoreArray[playerTurn] = newScore;
         return {
           ...state,
-          current_question_index: nextQuestionIndex,
-          score: score + 1,
+          // current_question_index: nextQuestionIndex,
+          score: scoreArray,
+          playerTurn: playerTurn + 1,
         };
       } else {
-        return { ...state, current_question_index: nextQuestionIndex };
+        const scoreArray = [...state.score];
+        const newScore = state.score[playerTurn] ? state.score[playerTurn] : 0;
+        scoreArray[playerTurn] = newScore;
+        // return { ...state, current_question_index: nextQuestionIndex };
+        return { ...state, score: scoreArray, playerTurn: playerTurn + 1 };
       }
 
+
     case "END_QUESTIONS":
+      console.log(action.payload);
       if (chosenAnswer === correctAnswer) {
-        return { ...state, score: score + 1 };
+        const scoreArray = [...state.score];
+        const newScore = state.score[playerTurn]
+          ? state.score[playerTurn] + 1
+          : 1;
+        scoreArray[playerTurn] = newScore;
+        return { ...state, score: scoreArray, playerTurn: playerTurn + 1 };
       } else {
-        return { ...state };
+        const scoreArray = [...state.score];
+        const newScore = state.score[playerTurn] ? state.score[playerTurn] : 0;
+        scoreArray[playerTurn] = newScore;
+        return { ...state, score: scoreArray, playerTurn: playerTurn + 1 };
       }
+
+    case "NEXT_QUESTION":
+      console.log(action.payload);
+      console.log("next question");
+      return {
+        ...state,
+        current_question_index: nextQuestionIndex,
+        playerTurn: playerTurn,
+      };
 
     case "GET_SCORE":
       return { ...state };
